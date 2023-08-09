5553fcb265ca643dbcfaffad5555f830733892b5
Xianyi Nie <xnie40@gatech.edu>
2023-08-09 16:33:01 -0400

    Finished the skeleton of the project

--- backend/src/main/java/com/kob/backend/contoller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/contoller/pk/IndexController.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class IndexController {
     @RequestMapping("index/")
-    public String index() {
-        return "hhhh";
+    public Map<String, String> index() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name","apple");
+        map.put("rating", "1000");
+        return map;
     }
 }

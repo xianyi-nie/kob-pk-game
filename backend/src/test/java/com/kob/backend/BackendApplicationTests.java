c1b7446f4366fdeb2a1b60f4246f213c9bc4d6fa
Xianyi Nie <xnie40@gatech.edu>
2023-08-11 21:26:35 -0400

    Config MySQL Module and Add User Login Module

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,24 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("pnxy"));
+        System.out.println(passwordEncoder.encode("pb"));
+        System.out.println(passwordEncoder.encode("pc"));
+        System.out.println(passwordEncoder.encode("pd"));
+
+        System.out.println(passwordEncoder.matches("pnxy",
+                "$2a$10$BXDajimtRkPhNivXlktoh.Lg0Gj9ooQ92hnfEiUWdym9IoGKiM7dq"));
+
     }
 
 }
